69a43e3cea842253ffe01d95f38362dfe71076e7
melissadrrs <melissadrrs@gmail.com>
2022-06-23 17:19:27 -0500

    commit 2

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var url = require('url');
+
+function mensaje(req, resp) {
+    var parsedUrl = url.parse(req.url, true);
+    var query = parsedUrl.query;
+	resp.writeHead(200, {'content-type': 'text/plain'});
+	resp.write(`Hola Mundo duplicaste : ${formula(query.number)}`);
+	resp.end();
+}
+
+function formula(number){
+ return number*2
+}
+
+module.exports = { mensaje, formula}
